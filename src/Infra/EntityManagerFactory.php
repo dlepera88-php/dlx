482b5e1b5bbf7e2cd1a626b0780c135dfd5eb7a3
dlepera <dlepera88@gmail.com>
2019-04-28 19:08:53 -0300

    Incluindo suporte para types do doctrine

--- src/Infra/EntityManagerFactory.php
+++ src/Infra/EntityManagerFactory.php
@@ -9,9 +9,15 @@
 namespace DLX\Infra;
 
 use DLX\Infra\Exceptions\EntityManagerNaoEncontradoException;
+use Doctrine\DBAL\DBALException;
 use Doctrine\DBAL\Logging\EchoSQLLogger;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMException;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager as DoctrineEntityManager;
+use Exception;
 
 class EntityManagerFactory
 {
@@ -23,32 +29,67 @@ class EntityManagerFactory
      * @param array $conexao
      * @param array $config
      * @return DoctrineEntityManager
-     * @throws \Doctrine\ORM\ORMException
-     * @throws \Exception
+     * @throws ORMException
+     * @throws Exception
      */
     public static function create(string $tipo_em, array $conexao, array $config)
     {
         switch ($tipo_em) {
             case self::ORM_DOCTRINE:
-                switch ($config['mapping']) {
-                    case 'yaml':
-                        $doctrine_config = Setup::createYAMLMetadataConfiguration($config['dir'], (bool)$config['dev-mode']);
-                        break;
-                    case 'xml':
-                        $doctrine_config = Setup::createXMLMetadataConfiguration($config['dir'], $config['dev-mode']);
-                        break;
-                    default:
-                        $doctrine_config = Setup::createAnnotationMetadataConfiguration($config['dir'], $config['dev-mode']);
-                }
+                $doctrine_config = self::getDoctrineConfig($config['mapping'], $config['dir'], $config['dev-mode']);
 
                 if (array_key_exists('debug', $config) && $config['debug']) {
                     $doctrine_config->setSQLLogger(new $config['debug']);
                 }
 
-                return DoctrineEntityManager::create($conexao, $doctrine_config);
+                /** @var EntityManager $em */
+                $em = DoctrineEntityManager::create($conexao, $doctrine_config);
+
+                if (array_key_exists('types', $config)) {
+                    self::adicionarDoctrineTypes($em, $config['types']);
+                }
+
+                return $em;
                 break;
             default:
                 throw new EntityManagerNaoEncontradoException($tipo_em);
+        }
+    }
+
+    /**
+     * Retorna a configuração necessária do doctrine para gerar o Entity Manager
+     * @param string $tipo
+     * @param array $dir
+     * @param bool $dev_mode
+     * @return Configuration
+     */
+    private static function getDoctrineConfig(string $tipo, array $dir, bool $dev_mode = true): Configuration
+    {
+        switch ($tipo) {
+            case 'yaml':
+                $doctrine_config = Setup::createYAMLMetadataConfiguration($dir, $dev_mode);
+                break;
+            case 'xml':
+                $doctrine_config = Setup::createXMLMetadataConfiguration($dir, $dev_mode);
+                break;
+            default:
+                $doctrine_config = Setup::createAnnotationMetadataConfiguration($dir, $dev_mode);
+        }
+
+        return $doctrine_config;
+    }
+
+    /**
+     * Adicionar os tipos de dados no doctrine
+     * @param DoctrineEntityManager $em
+     * @param array $types
+     * @throws DBALException
+     */
+    private static function adicionarDoctrineTypes(EntityManager $em, array $types): void
+    {
+        foreach ($types as $nome => $type) {
+            Type::addType($nome, $type);
+            $em->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping($nome, $nome);
         }
     }
 }
